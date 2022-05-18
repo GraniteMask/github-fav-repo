64019a7c5f53c89d15f9c92e17723945a1c7ed7f
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-18 21:40:08 +0530

    reducers

--- src/reducers/searchReducers.js
+++ src/reducers/searchReducers.js
@@ -1,2 +1,15 @@
 import { SEARCH_SUCCESS, SEARCH_FAIL, SEARCH_REQUEST } from "../constants/constants";
 
+export const searchReducer = (state={results:{}, loading: true}, action) =>{
+    switch(action.type){
+        case SEARCH_REQUEST:
+            return {...state, loading: true}
+        case SEARCH_SUCCESS:
+            return {...state, loading: false, results: action.payload}
+        case SEARCH_FAIL:
+            return {...state, loading: false, error:action.payload}
+        default:
+            return state
+    }
+    
+}
