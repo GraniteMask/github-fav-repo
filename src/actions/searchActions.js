74cb20fdf7d0c89a0bce0bfd323ff57c16ba62bb
GraniteMask <ratnadeepastronomy@gmail.com>
2022-05-18 21:36:41 +0530

    actions

--- src/actions/searchActions.js
+++ src/actions/searchActions.js
@@ -2,11 +2,10 @@ import axios from "axios";
 import { SEARCH_SUCCESS, SEARCH_FAIL, SEARCH_REQUEST } from "../constants/constants";
 
 export const searchRepo = (query) => async(dispatch) =>{
-    dispatch({
-        type: SEARCH_REQUEST
-    })
+    dispatch({ type: SEARCH_REQUEST })
     try{
-        const {data} = await axios.get(``)
+        const {data} = await axios.get(`https://api.github.com/search/repositories?q=${query}`)
+        dispatch({type: SEARCH_SUCCESS, payload: data})
     }catch(error){
         dispatch({type: SEARCH_FAIL, payload:error.message})
     }
